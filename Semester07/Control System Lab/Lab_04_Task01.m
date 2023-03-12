1df6c6e0fd0018aaebb7759091cd733fb31800ed
Amir Suliman <amirsuliman01@gmail.com>
2023-03-12 13:03:23 +0500

    Delete Semester07 directory

--- Semester07/Control System Lab/Lab_04_Task01.m
+++
@@ -1,8 +0,0 @@
-clc
-clear all
-close all
-
-Num1 = 1;
-Den1 = [1 1];
-
-sys1 = ft(Num1, Den1);
--- Semester07/Control System Lab/Lab_08_task03.m
+++
@@ -1,9 +0,0 @@
-%find residues of the following
-% F(s) = 5/(s+1)(s+2)(s+3)
-
-clc
-clear all
-close all
-num = 5;
-denum = [1,6,11,6];
-[R,P,K] = residue(num,denum)
